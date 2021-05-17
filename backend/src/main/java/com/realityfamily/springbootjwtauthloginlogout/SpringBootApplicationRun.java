0261ada703fc692a1c8fd0c1bed32f104606a5a3
Artem <artemmakarevich1997@gmail.com>
2021-05-18 01:32:14 +0300

    Initial commit

---
+++ backend/src/main/java/com/realityfamily/springbootjwtauthloginlogout/SpringBootApplicationRun.java
@@ -0,0 +1,13 @@
+package com.realityfamily.springbootjwtauthloginlogout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationRun {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationRun.class, args);
+	}
+
+}
